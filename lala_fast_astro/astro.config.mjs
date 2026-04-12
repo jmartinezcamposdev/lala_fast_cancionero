8f89f03960b8ca1388ca6f97703261b5a9f94767
Javier Martínez <jmartinezcampos98@gmail.com>
2026-04-12 15:54:51 +0200

    feat: Add Netlify support

--- lala_fast_astro/astro.config.mjs
+++ lala_fast_astro/astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   output: 'server',
-  adapter: vercel({
+  adapter: netlify({
     includeFiles: ['./src/data/lala_data.db'],
-  }),
+  })
 });
